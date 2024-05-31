01730b3b0fbcfda16b636767d42ae4ad46bb0a9a
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2024-05-31 16:56:39 +0530

    Add files via upload

---
+++ video2.py
@@ -0,0 +1,17 @@
+import cv2
+import mediapipe as mp
+import time
+cap=cv2.VideoCapture(0)
+mpHands=mp.solutions.hands
+hands=mpHands.Hands()
+mpDraw=mp.solutions.drawing_utils
+while(1):
+    success,img=cap.read()
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results=hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+       for handLms in results.multi_hand_landmarks:
+           mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+        
+    cv2.imshow("video",img)
+    cv2.waitKey(1)
